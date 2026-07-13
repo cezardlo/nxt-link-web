0e010d4780e21a329a73bcdf7d5b2434269de7a4
cezardlo <cdelao@miners.utep.edu>
2026-07-13 16:22:44 -0600

    add company profile loading state

---
+++ src/app/company-profile/loading.tsx
@@ -0,0 +1,10 @@
+export default function CompanyProfileLoading() {
+  return (
+    <main className="min-h-screen bg-nxt-bg px-4 py-8 text-nxt-text sm:px-6">
+      <div className="mx-auto max-w-5xl animate-pulse space-y-6">
+        <div className="h-72 rounded-[2rem] border border-white/[0.08] bg-white/[0.025]" />
+        <div className="h-[720px] rounded-[2rem] border border-white/[0.08] bg-white/[0.025]" />
+      </div>
+    </main>
+  );
+}
